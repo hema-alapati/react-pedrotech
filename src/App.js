cccf7b955207828f58aa43f19e883a92e015332f
Hemalatha <hemachowdary2744@gmail.com>
2024-02-27 10:47:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    return (
+    <div className={styles.App}>
+      <div className={styles.name}>Pedro</div>
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google"/>
+      <Job salary={10000} position="Project Manager" company="Netflix"/>
     </div>
-  );
+    );
+}
+
+const Job = (props) =>{
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  ); 
 }
 
 export default App;
