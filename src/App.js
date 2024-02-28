4b1e949f1f8a68111e4f846a1b071324b1a96b90
Hemalatha <hemachowdary2744@gmail.com>
2024-02-28 16:54:16 +0530

    cat fact exercise, useEffect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import "./App.css";
 import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  // fetch("https://catfact.ninja/fact").then((res) =>
-  //   res.json().then((data) => {
-  //     console.log(data);
-  //   })
-  // );
+  const [catFact, setCatFact] = useState("");
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  };
 
-  Axios.get("https://catfact.ninja/fact").then((res) => {
-    console.log(res.data);
-  });
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
 
   return (
     <div className="Ap p">
       <button>Generate Cat Fact</button>
-      <p></p>
+      <p>{catFact}</p>
     </div>
   );
 }
