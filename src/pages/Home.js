0538726f3d5f2b595242cfd420dc7a443d324d35
Hemalatha <hemachowdary2744@gmail.com>
2024-02-29 18:39:16 +0530

    react query (issue)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
 
 export const Home = () => {
-  const { data, isLoading } = useQuery(["cat"], () => {
+  const {
+    data: catData,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery(["cat"], () => {
     return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
   });
+
+  if (isError) {
+    return <h1>Sorry, there was an error</h1>;
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -12,7 +21,7 @@ export const Home = () => {
 
   return (
     <h1>
-      THIS IS THE HOME PAGE <p>{data?.fact}</p>
+      THIS IS THE HOME PAGE <p>{catData?.fact}</p>
     </h1>
   );
 };
