7b272b98be5b67a0073daa81adbe1d37b4f634dd
Hemalatha <hemachowdary2744@gmail.com>
2024-02-29 18:23:08 +0530

    broken

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const client = new QueryClient();
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<h1>Page not found</h1>} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1>Page not found</h1>} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
+
 export const Home = () => {
-  return <h1>THIS IS THE HOME PAGE</h1>;
+  const { data, isLoading } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <h1>
+      THIS IS THE HOME PAGE <p>{data?.fact}</p>
+    </h1>
+  );
 };
